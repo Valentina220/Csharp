27c91b21bfa4d86a1cee65c13d9cd6e3c192a5b1
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-10-20 22:51:43 +0300

    Add files via upload

---
+++ TriangleArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea
+{
+    class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first side of triangle");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second side of triangle");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the third side of triangle");
+            double c = double.Parse(Console.ReadLine());
+
+            double p = a + b + c;
+            double S = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            Console.WriteLine("Периметр\t\tПлощадь");
+            Console.WriteLine($"{p:.##}" + "\t\t" + $"{S:.##}"); //("Площадь треуголинка S= ", S);
+        }
+    }
+}
