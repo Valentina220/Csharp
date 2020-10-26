4d9d840ed6c6c01cf7e53334d3b18f3a9ff706d1
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-10-26 23:41:39 +0300

    Add files via upload

---
+++ EquationSolution.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    class EquationSolution
+    {
+
+        //public struct RootsOfEq
+        //{
+        //    public double x1;
+        //    public double x2;
+        //    public bool checkroots;
+        //}
+
+        //public static double RootsFinding(double a, double b, double c, out RootsOfEq) //out QuadraticEquation.EquationSolution.RootsOfEq) //out double x1, out double x2)
+        //{
+        //    //x1 = 0;
+        //    //x2 = 0;
+
+        //    RootsOfEq root, check;
+
+        //    double D = b * b - 4 * a * c;
+
+
+        //    if (D > 0)
+        //    {
+        //        check.checkroots = true;
+        //        root.x1 = (b * b + Math.Sqrt(D)) / (2 * a);
+        //        root.x2 = (b * b - Math.Sqrt(D)) / (2 * a);
+        //    }
+        //    else if (D == 0)
+        //    {
+        //        check.checkroots = true;
+        //        root.x1 = root.x2 = -b / (2 * a);
+        //    }
+        //    else
+        //    {
+        //        check.checkroots = false;
+        //        //Console.WriteLine("Корней нет")
+        //            //return 0;
+        //    }
+
+        //}
+
+
+        public static double Checking(double a, double b, double c, out int checkres, out double x1, out double x2)
+        {
+            x1 = 0;
+            x2 = 0;
+
+            double D = b * b - 4 * a * c;
+
+            if (D > 0)
+            {
+                checkres = 1;
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);                
+               
+            }
+            else if (D == 0)
+            {
+                checkres = 0;
+                x1 = x2 = -b / (2 * a);
+            }
+            else
+            {
+                checkres = -1;
+            }
+            return checkres;
+           
+                
+        }
+
+      
+    }
+}
---
+++ Quadr.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    class Quadr
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты квадратного уравнения");
+            Console.WriteLine("a= ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("b= ");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("c= ");
+            double z = double.Parse(Console.ReadLine());
+
+            
+
+            double check = EquationSolution.Checking(x, y, z, out int checkres, out double x1, out double x2);
+
+            if (check == 1)
+            {
+                Console.WriteLine("Корни уравнения с коэффициентами a = {0}, b = {1}, c = {2} равны: x1 = {3}, x2 = {4}", +x, +y, +z, +x1, +x2);
+
+            }
+            else if (check == 0)
+            {
+                Console.WriteLine("Корень уравнения с коэффициентами a = {0}, b = {1}, c = {2} один: x1 = x2 = {3}", +x, +y, +z, +x1);
+            }
+            else
+            {
+                Console.WriteLine("Корней уравнения с коэффициентами a = {0}, b = {1}, c = {2} нет",+x, +y, +z);
+            }
+
+            //Console.WriteLine(check);
+            
+        }
+    }
+}
