dd0252b41ddb517a47a92ec15b6e4a90ab36c966
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-10-30 21:58:29 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+
+    class Book : IComparable
+    {
+        private string author;
+        private string title;
+        private int pages;
+        private int year;
+
+
+        public Book(String author, String title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+            Console.WriteLine(author + title + pages + year);
+        }
+
+        public override string ToString()
+        {
+            return author + " " + title + " " + pages + " " + year;
+        }
+
+
+        int IComparable.CompareTo(object obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\n {2} стр.\n Год издания: {3}\n ", author, title, pages, year);
+        }
+
+    }    
+}
---
+++ ProgramBook.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book Bk1 = new Book("Оруэл Дж ", "Скотный двор ", 256 , 2019);
+            //Console.WriteLine(Bk1.ToString());
+            Book Bk2 = new Book("Сартр Ж.П.", "Стена", 285, 2017);
+            //Console.WriteLine(Bk2.ToString());
+            Book Bk3 = new Book("Фромм Э. ", "Иметь или быть? ", 320 , 2020);
+            //Console.WriteLine(Bk3.ToString());
+
+            Bk1.Show();
+            Bk2.Show();
+            Bk3.Show();
+
+            //Console.WriteLine("Введите количество книг:");
+            //int num = int.Parse(Console.ReadLine());
+            //int[] a = new int[num];
+
+            //Console.WriteLine("Введите данные о книгах: ");
+
+            //for (int i = 0; i < a.GetLength(0); i++) 
+            //{
+            //    Console.WriteLine("Введите имя автора: ");
+            //    string author = (Console.ReadLine());
+            //    Console.WriteLine("Введите название книги: ");
+            //    string title = (Console.ReadLine());
+            //    Console.WriteLine("Введите количество страниц: ");
+            //    string pages = (Console.ReadLine());
+            //    Console.WriteLine("Введите год издания: ");
+            //    string year = (Console.ReadLine());
+
+            //    Bk.SetBook(author, title, pages, year);
+            //    Bk.Sort();
+            //    Bk.CompareTo();
+            //}
+            Book[] arrBook = { Bk1, Bk2, Bk3 };
+            Array.Sort(arrBook);
+            foreach (var item in arrBook)
+            {
+                Console.WriteLine(item);
+            }
+
+            //static string CompareAuthors(Book Book1, Person Book2)
+            //{
+            //    bool equalParams = person1.Equals(person2);
+            //    Object personobj = person1;
+            //    bool fullEqual = personobj.Equals(person2);
+
+            //    if (fullEqual)
+            //        return "Абсолютно одинаковые объекты";
+            //    if (equalParams)
+            //        return "Одинаковые свойства объектов";
+
+            //    return "Объекты разные";
+            //}
+        }
+    }
+
+    
+
+}
+
