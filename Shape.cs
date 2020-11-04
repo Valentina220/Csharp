8667c1f083501f86a0332ab00052af8ad68ceeb3
Valentina220 <73115404+Valentina220@users.noreply.github.com>
2020-11-04 12:09:12 +0300

    Add files via upload

---
+++ Shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassShape
+{
+
+    interface IRotate
+    {
+        string Rotate();
+    }
+
+
+    abstract class Shape
+    {
+        public abstract string ClassName();
+        public abstract double Area();   
+
+        public abstract double Perimetr();
+
+        public override string ToString()
+        {
+            string res = "Периметр равен = " + Perimetr() +"\n" + "Площадь равна = " + Area();
+            return res;
+        }
+
+       // public abstract string Rotate();
+    }
+
+}
